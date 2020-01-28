93b24072c6570d495fba190b130954dbb67cf3a7
João Vitor <joaovitorcoelho13@gmail.com>
2020-01-28 07:32:38 -0300

    chore: add sintax sucrase

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,9 @@
-const {Router} = require('express');
+import {Router} from 'express';
 
 const routes = new Router();
 
 routes.get(('/'),(req, res)=>{
-    res.json({message: "Hello Gostack"});
+    res.json({message: "Hello F"});
 })
 
-module.exports = routes;
+export default routes;
