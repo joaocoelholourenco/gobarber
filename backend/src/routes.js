fbf327a9f3c69da2dc42b2cd6fa730fc4f5126f6
João Vitor <joaovitorcoelho13@gmail.com>
2020-01-28 13:01:16 -0300

    Error

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello joao' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'João Vitor',
+    email: 'joaovitorcoelho13@gmail.com',
+    password_hash: '1254631542',
+  });
+  return res.json(user);
 });
 
 export default routes;
