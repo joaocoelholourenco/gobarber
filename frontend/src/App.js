1f06255bd2f3c308c2c5306906a6b6c55d498397
João Vitor <joaovitorcoelho13@gmail.com>
2020-06-01 12:06:00 -0300

    add config redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 
 import './config/ReactrotonConfig';
+import store from './store';
 
 import Routes from './routes';
 import history from './services/history';
 
 function App() {
   return (
-    <Router history={history}>
-      <GlobalStyle />
-      <Routes />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <GlobalStyle />
+        <Routes />
+      </Router>
+    </Provider>
   );
 }
 
