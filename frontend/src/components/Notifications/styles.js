22ef441abb19cbfa9d9c4440aa176af95c820b8a
João Vitor <joaovitorcoelho13@gmail.com>
2020-06-03 15:01:58 -0300

    add resquest notifications

--- frontend/src/components/Notifications/styles.js
+++ frontend/src/components/Notifications/styles.js
@@ -33,7 +33,8 @@ export const NotificationList = styled.div`
   top: calc(100% + 30px);
   background: rgba(0, 0, 0, 0.6);
   border-radius: 4px;
-  padding: 15px; 5px;
+  padding: 15px 5px;
+  display: ${(props) => (props.visible ? 'block' : 'none')};
 
   &::before {
     content: '';
@@ -63,8 +64,10 @@ export const Notification = styled.div`
   }
 
   time {
+    display: block;
     font-size: 12px;
     opacity: 0.6;
+    margin-bottom: 5px;
   }
 
   button {
@@ -74,7 +77,6 @@ export const Notification = styled.div`
     color: ${lighten(0.2, '#7159c1')};
     padding: 0 5px;
     margin: 0 5px;
-    border-left: 1px solid rgba(255, 255, 255, 0.1);
   }
 
   ${(props) =>
@@ -87,6 +89,7 @@ export const Notification = styled.div`
         height: 8px;
         background: #ff892e;
         border-radius: 50%;
+        margin-left: 10px;
       }
     `}
 `;
