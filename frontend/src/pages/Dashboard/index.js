efa36a2a50b16654a1a0c8fa1e7f3939c0b35450
João Vitor <joaovitorcoelho13@gmail.com>
2020-06-15 14:41:41 -0300

    added dashboard styled

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
-function Dashboard() {
-  api.get('appointments');
-  return <h1>Dashboard</h1>;
+export default function Dashboard() {
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em Aberto</span>
+        </Time>
+        <Time>
+          <strong>08:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+        <Time>
+          <strong>08:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
-
-export default Dashboard;
